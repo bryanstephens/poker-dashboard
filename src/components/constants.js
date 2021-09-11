2d627f6082792acfc6c3cc4edf8c819bd91f79e7
Bryan Stephens <bryan.craig.stephens@gmail.com>
2021-09-11 11:32:27 -0700

    Reformat JS files using Prettier

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,15 +1,16 @@
 const pokerHandRankings = [
-          "Royal flush",
-          "Straight flush",
-          "Four of a kind",
-          "Full house",
-          "Flush",
-          "Straight",
-          "Three of a kind",
-          "Two pair",
-          "Pair",
-          "High card",
-]
+  " Royal flush",
+  "Straight flush",
+  "Four of a kind",
+  "Full house",
+  "Flush",
+  "Straight",
+  "Three of a kind",
+  "Two pair",
+  "Pair",
+  "High card",
+];
+
 export default {
     pokerHandRankings
 };
--- src/store/store.js
+++ src/store/store.js
@@ -1,38 +1,38 @@
-import { reactive, readonly } from 'vue';
+import { reactive, readonly } from "vue";
 
 const state = reactive({
-    bigBlind: 2560,
-    smallBlind: 1280,
-    players: [
-        {
-            name: 'Bryan',
-            timeOut: 500,
-            rank: '2nd',
-        },
-        {
-            name: 'Glenda',
-            timeOut: -1,
-            rank: '1st',
-        },
-    ],
-    highHand: {
-        title: 'Three of a kind',
-        pokerHandIndex: 6,
-        qualifier: 'Kings',
-        players: ['Glenda'],
+  bigBlind: 2560,
+  smallBlind: 1280,
+  players: [
+    {
+      name: "Bryan",
+      timeOut: 500,
+      rank: "2nd",
     },
-    firstFourOfAKind: {
-        players: ['Paolo']
-    }
+    {
+      name: "Glenda",
+      timeOut: -1,
+      rank: "1st",
+    },
+  ],
+  highHand: {
+    title: "Three of a kind",
+    pokerHandIndex: 6,
+    qualifier: "Kings",
+    players: ["Glenda"],
+  },
+  firstFourOfAKind: {
+    players: ["Paolo"],
+  },
 });
 
 const getters = {
-    getPlayers() {
-        return [...state.players];
-    }
-}
+  getPlayers() {
+    return [...state.players];
+  },
+};
 
 export default {
-    state: readonly(state),
-    getters,
+  state: readonly(state),
+  getters,
 };
